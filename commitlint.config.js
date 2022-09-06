56023e77ef69ea56dbb05450ab5b76c48f68efdf
Amin Zibayi <7developer2@gmail.com>
2022-09-07 03:04:16 +0430

    Dev: setup commit lint

---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+/* بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِیمِ
+الّلهُمَّ صَلِّ عَلی مُحَمَّد وَآلِ مُحَمَّد وَعَجِّل فَرَجَهُم */
+
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  parserPreset: "conventional-changelog-conventionalcommits",
+  formatter: "@commitlint/format",
+  rules: {
+    "type-case": [2, "always", "pascal-case"],
+    "type-enum": [
+      2,
+      "always",
+      ["Docs", "Feat", "Fix", "Refactor", "Test", "Dev", "Clean"],
+    ],
+  },
+};
