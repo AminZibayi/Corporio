7304f94b179eca8bbc758186b51da70b0c6da7d1
Amin Zibayi <7developer2@gmail.com>
2022-09-15 20:59:01 +0430

    Docs: add TODO to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,20 @@
+// In the Name of God, the Originator the Creative
+
 // app.js for concatenation of smaller libraryies
 // to reduce http requests of small files
-'use strict';
+"use strict";
 
 // Prefetch in-viewport links during idle time
-import { listen } from 'quicklink/dist/quicklink.mjs';
+import { listen } from "quicklink/dist/quicklink.mjs";
 listen();
 
 // lazy sizes for image loading
-import 'lazysizes';
+// TODO: use lazysizes in all pages
+import "lazysizes";
 
 // global alert
-import './assets/js/alert';
+import "./assets/js/alert";
+
+import "./script";
+
+import "./form-handler";
