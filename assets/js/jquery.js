825dc0a3b5dbcf2681301b7ccee4acb2f5384390
Amin Zibayi <7developer2@gmail.com>
2022-09-18 00:09:43 +0430

    Feat: add jQuery

---
+++ assets/js/jquery.js
@@ -0,0 +1,7 @@
+// In the Name of God, the Originator the Creative
+
+// TODO: Remove jquery and use vanilla javascript
+import $ from "jquery";
+import "magnific-popup/dist/jquery.magnific-popup";
+
+window.jQuery = window.$ = $;
