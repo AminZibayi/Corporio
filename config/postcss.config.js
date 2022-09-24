6674ff23325e5330dc366663aa80b9abfb11f6f6
Amin Zibayi <7developer2@gmail.com>
2022-09-24 23:01:16 +0330

    Fix: disable `purgeCSS`

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,29 +1,32 @@
+// In the Name of God, the Originator the Creative
+
 const autoprefixer = require("autoprefixer");
-const purgecss = require("@fullhuman/postcss-purgecss");
-const whitelister = require("purgecss-whitelister");
+// const purgecss = require("@fullhuman/postcss-purgecss");
+// const whitelister = require("purgecss-whitelister");
 
 module.exports = {
   plugins: [
     autoprefixer(),
-    purgecss({
-      content: ["./hugo_stats.json"],
-      safelist: [
-        "lazyloaded",
-        "table",
-        "thead",
-        "tbody",
-        "tr",
-        "th",
-        "td",
-        "h3",
-        "alert-link",
-        ...whitelister([
-          "./assets/scss/components/_alerts.scss",
-          // './assets/scss/components/_buttons.scss',
-          "./assets/scss/components/_code.scss",
-          // './assets/scss/components/_syntax.scss',
-        ]),
-      ],
-    }),
+    // TODO: Purgecss removes some necessary stylesheets, fix it and uncomment it (this is probably because of jquery injected DOM)
+    // purgecss({
+    //   content: ["./hugo_stats.json"],
+    //   safelist: [
+    //     "lazyloaded",
+    //     "table",
+    //     "thead",
+    //     "tbody",
+    //     "tr",
+    //     "th",
+    //     "td",
+    //     "h3",
+    //     "alert-link",
+    //     ...whitelister([
+    //       "./assets/scss/components/_alerts.scss",
+    //       // './assets/scss/components/_buttons.scss',
+    //       "/assets/scss/_slick.scss",
+    //       "/assets/scss/_magnific_popup.scss",
+    //     ]),
+    //   ],
+    // }),
   ],
 };
